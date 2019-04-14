03db90918b8bd3c1995eb16a7449f27bd5e0f65c
Beto Dute <betodute@gmail.com>
2019-04-14 23:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var storageArray = [];
 for (var i = 0; i < musicians.length; i++) {
-storageArray.push(`${musicians} plays ${guitar}`);
+storageArray.push(musicians[i] + " plays " + instruments[i]);
 }
 return storageArray;
 }
